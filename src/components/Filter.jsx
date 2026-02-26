b860564e7bf7bdffedc3337c97af3c05793aa0fe
Niraj <nierforprogramming@gmail.com>
2026-02-26 20:50:28 +0545

    Fixed width of drop down

--- src/components/Filter.jsx
+++ src/components/Filter.jsx
@@ -1,31 +1,42 @@
 import { CiFilter } from "react-icons/ci";
 import { filterByStatus } from "../assets";
+import { useState } from "react";
 
 const Filter = ({ filterText, handleFilter }) => {
+  const [open, setOpen] = useState(false);
+
   return (
     <>
-      <div className="relative">
-        <div className="bg-white rounded-sm px-4 cursor-pointer flex items-center space-x-2 py-2">
+      <div className="relative w-50">
+        <div
+          onClick={() => setOpen((prev) => !prev)}
+          className="bg-white rounded-sm px-4 cursor-pointer flex items-center space-x-2 py-2"
+        >
           <span>
             <CiFilter />
           </span>
           <button className="cursor-pointer font-semibold">{filterText}</button>
         </div>
 
-        <div className="bg-white absolute py-4 top-12 -left-30 shadow-md">
-          <ul className="ml-2">
-            <div className="font-semibold  px-2">Filter by Status</div>
-            {filterByStatus.map((status, index) => (
-              <li
-                onClick={(event) => handleFilter(event)}
-                className={`hover:bg-gray-100 w-full px-2 ${status == filterText ? "font-semibold" : ""}`}
-                key={index}
-              >
-                {status}
-              </li>
-            ))}
-          </ul>
-        </div>
+        {open && (
+          <div className="bg-white absolute py-4 top-12 w-full shadow-md">
+            <ul className="ml-2">
+              <div className="font-semibold  px-2">Filter by Status</div>
+              {filterByStatus.map((status, index) => (
+                <li
+                  onClick={(event) => {
+                    handleFilter(event);
+                    setOpen(false);
+                  }}
+                  className={`hover:bg-gray-100 w-full px-2 ${status == filterText ? "font-semibold" : ""}`}
+                  key={index}
+                >
+                  {status}
+                </li>
+              ))}
+            </ul>
+          </div>
+        )}
       </div>
     </>
   );
